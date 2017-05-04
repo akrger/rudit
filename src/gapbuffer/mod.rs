0bb3d47b2af394cfcab4fe96e62eafeb014640cb
André Krüger <zeldamer@t-online.de>
2017-05-04 06:39:48 +0200

    Switch to pancurses

--- src/gapbuffer/mod.rs
+++ src/gapbuffer/mod.rs
@@ -3,35 +3,40 @@ use std::fmt::{Formatter, Result};
 
 pub struct GapBuffer {
     pub buffer: Vec<char>,
-    cursor: usize,
+    pub cursor: usize,
     pub gap_start: usize,
     gap_end: usize,
 }
 
 impl GapBuffer {
     fn check_gap(&mut self) {
-        if self.gap_start == self.gap_end {
+        // buffer voll
+        if self.gap_end == self.gap_start {
             let len = self.buffer.clone().len();
-            self.buffer.resize(len * 2, ' ')
+            self.buffer.resize(len * 2, ' ');
+            self.gap_end = self.buffer.len();
         }
+        // links vom cursor
+
+        // rechts vom cursor
     }
 
-    pub fn insert(&mut self, ch: char) {
+    pub fn insert(&mut self, index: usize, ch: char) {
         // verursacht, dass der cursor zu weit
         // vorspringt
-        //self.check_gap();
-        self.buffer[self.cursor] = ch;
+        self.check_gap();
+        self.buffer[self.gap_start] = ch;
         self.gap_start += 1
     }
     pub fn delete(&mut self) {
-        self.buffer[self.cursor] = ' ';
+        self.buffer[self.cursor] = '\0';
         self.gap_start -= 1
     }
-    pub fn new() -> GapBuffer {
+    pub fn new(size: usize) -> GapBuffer {
         GapBuffer {
-            buffer: vec!['\0'; 10],
+            buffer: vec!['\0'; size],
             gap_start: 0,
-            gap_end: 9,
+            gap_end: size - 1,
             cursor: 0,
         }
     }
@@ -48,5 +53,49 @@ impl Display for GapBuffer {
 #[cfg(test)]
 mod tests {
     #[test]
-    fn test() {}
+    fn test_insert() {
+        use gapbuffer::GapBuffer;
+        let mut buffer = GapBuffer::new(5);
+        buffer.insert(0, 'a');
+        assert_eq!('a', buffer.buffer[0]);
+    }
+    #[test]
+    fn test_insert2() {
+        use gapbuffer::GapBuffer;
+        let mut buffer = GapBuffer::new(2);
+        buffer.insert(0, 'a');
+        assert_eq!(2, buffer.buffer.len());
+        buffer.insert(1, 'c');
+        buffer.insert(2, 'b');
+        assert_eq!('a', buffer.buffer[0]);
+        assert_eq!('b', buffer.buffer[2]);
+        assert_eq!(4, buffer.buffer.len());
+        assert_eq!(3, buffer.gap_start);
+        assert_eq!(4, buffer.gap_end);
+    }
+    #[test]
+    fn test_insert3() {
+        use gapbuffer::GapBuffer;
+        let mut buffer = GapBuffer::new(5);
+        buffer.insert(0, 't');
+        buffer.insert(1, 'h');
+        buffer.insert(2, 'e');
+        assert_eq!(1, buffer.gap_end - buffer.gap_start);
+        assert_eq!(3, buffer.gap_start);
+        assert_eq!(4, buffer.gap_end);
+        assert_eq!(5, buffer.buffer.len());
+        buffer.insert(3, ' ');
+        buffer.insert(4, 'f');
+        assert_eq!(10, buffer.buffer.len());
+        buffer.insert(5, 'o');
+        buffer.insert(6, 'x');
+        // insert before f
+        buffer.insert(4, 'w');
+        buffer.insert(5, 'h');
+        buffer.insert(6, 'i');
+        buffer.insert(7, 't');
+        buffer.insert(8, 'e');
+        assert_eq!('w', buffer.buffer[4]);
+        assert_eq!('f', buffer.buffer[9]);
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,70 @@
-extern crate termion;
 extern crate rudit;
-
-use termion::raw::IntoRawMode;
-use termion::event::Key;
-use termion::input::TermRead;
-use std::io::{Write, stdin, stdout};
-
+extern crate pancurses;
+use pancurses::*;
 use rudit::gapbuffer::GapBuffer;
-
 fn main() {
-    let mut buffer = GapBuffer::new();
-    let stdin = stdin();
-    let mut stdout = stdout().into_raw_mode().unwrap();
-    write!(stdout,
-           "{}{}",
-           termion::cursor::Goto(1, 1),
-           termion::clear::All)
-            .unwrap();
-    stdout.flush().unwrap();
-    for c in stdin.keys() {
-        stdout.flush().unwrap();
-        match c.unwrap() {
-            Key::Char('q') => break,
-            Key::Char('\n') => {
-                buffer.insert('\n');
-                write!(stdout, "{}", termion::cursor::Down(1)).unwrap();
-                write!(stdout, "{}", termion::cursor::Left(buffer.gap_start as u16)).unwrap();
-                stdout.flush().unwrap();
-                ()
+    let mut buffer = GapBuffer::new(10);
+    buffer.insert(0, 'b');
+    buffer.insert(1, 'a');
+    // buffer.insert(2, 'a');
+    // buffer.insert(3, 'b');
+    let mut stdscr = initscr();
+    stdscr.keypad(true);
+    noecho();
+    nl();
+    cbreak();
+    let mut cx = 0;
+    let mut cy = 0;
+    loop {
+        draw_buffer(buffer.buffer.clone(), &mut stdscr);
+        move_cursor(cx, cy, &mut stdscr);
+        stdscr.refresh();
+        match stdscr.getch() {
+            Some(Input::Character(c)) => {
+                if c == '\n' {
+                    cy += 1;
+                    cx = 0;
+                    buffer.insert(cx as usize, '\n');
+                    continue;
+                }
+                buffer.insert(cx as usize, c);
+                cx += 1;
             }
-            Key::Char(c) => {
-                buffer.insert(c);
-                write!(stdout, "{}", buffer).unwrap();
-                stdout.flush().unwrap();
+            Some(Input::KeyEnter) => {
+                cy += 1;
+                cx = 0;
+                buffer.insert(cx as usize, '\n');
             }
-            Key::Up => {
-                write!(stdout, "{}", termion::cursor::Up(1)).unwrap();
-                stdout.flush().unwrap();
-                ()
+            Some(Input::KeyRight) => {
+                cx += 1;
             }
-            Key::Left => {
-                write!(stdout, "{}", termion::cursor::Left(1)).unwrap();
-                stdout.flush().unwrap();
-                ()
+            Some(Input::KeyLeft) => {
+                if cx > 0 {
+                    cx -= 1;
+                }
             }
-            Key::Right => {
-                write!(stdout, "{}", termion::cursor::Right(1)).unwrap();
-                stdout.flush().unwrap();
-                ();
-            }
+
             _ => (),
         }
     }
-    write!(stdout, "{}", termion::clear::All).unwrap();
-    stdout.flush().unwrap();
+    endwin();
+}
+fn draw_buffer(buffer: Vec<char>, stdscr: &mut Window) {
+    let mut x = 0;
+    let mut y = 0;
+    for c in buffer {
+        if c == '\n' {
+            y += 1;
+            x = 0;
+
+        } else if c == '\0' {
+
+        } else {
+            stdscr.mvaddch(y, x, c);
+            x += 1;
+        }
+    }
+}
+fn move_cursor(x: i32, y: i32, stdscr: &mut Window) {
+    stdscr.mv(y, x);
 }
