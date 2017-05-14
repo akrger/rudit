c201b9078a82c63eb2e7bc95d155612b602bfdfa
André Krüger <zeldamer@t-online.de>
2017-05-14 21:38:13 +0200

    start implement of line drawing

--- src/main.rs
+++ src/main.rs
@@ -11,18 +11,26 @@ fn main() {
     let mut buffer = GapBuffer::new(30);
     let mut stdout = stdout().into_raw_mode().unwrap();
     let mut index = 0;
-    let mut cx = 1;
-    let mut cy = 1;
+    let mut cx: u16 = 1;
+    let mut cy: u16 = 1;
     write!(stdout,
            "{}{}",
            termion::clear::All,
-           termion::cursor::Goto(cx as u16, cy as u16))
+           termion::cursor::Goto(cx, cy))
         .unwrap();
     stdout.flush().unwrap();
-    loop {
+    'main: loop {
+
+        write!(stdout, "{}", termion::clear::All).unwrap();
+        stdout.flush().unwrap();
+
+        draw_lines(&mut stdout);
+        draw_cursor(&mut stdout, cx, cy);
+
         for c in std::io::stdin().keys() {
             match c.unwrap() {
                 Key::Char('\n') => {
+                    buffer.insert(index, '\n');
                     cx = 1;
                     cy += 1;
                 }
@@ -37,17 +45,34 @@ fn main() {
                     }
                 }
                 Key::Right => {
-                    if cx <= buffer.len() {
+                    if cx as usize <= buffer.len() {
                         cx += 1
                     }
                 }
                 Key::Esc => break,
                 _ => (),
             }
-            // move cursor
-            write!(stdout, "{}", termion::cursor::Goto(cx as u16, cy as u16)).unwrap();
-            stdout.flush().unwrap();
+            continue 'main;
         }
+        write!(stdout, "{}", termion::clear::All).unwrap();
+        stdout.flush().unwrap();
         break;
     }
+}
+fn draw_lines(stdout: &mut termion::raw::RawTerminal<std::io::Stdout>) {
+    // TODO
+    // implement real drawing of buffer lines and line numbers
+    for i in 0..5 {
+        write!(stdout,
+               "{}{}",
+               termion::cursor::Goto(0, i + 1),
+               (i + 1).to_string())
+            .unwrap();
+    }
+    stdout.flush().unwrap();
+}
+
+fn draw_cursor(stdout: &mut termion::raw::RawTerminal<std::io::Stdout>, cx: u16, cy: u16) {
+    write!(stdout, "{}", termion::cursor::Goto(cx, cy)).unwrap();
+    stdout.flush().unwrap();
 }
