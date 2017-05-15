09d6b5d61d32ad4463f5dfe0fc563f38eb0eb7a0
André Krüger <zeldamer@t-online.de>
2017-05-15 16:21:23 +0200

    line drawing working

--- src/main.rs
+++ src/main.rs
@@ -5,34 +5,37 @@ use std::io::{Write, stdout};
 use termion::raw::IntoRawMode;
 use termion::event::Key;
 use termion::input::TermRead;
+use termion::cursor::Goto;
 use rudit::gapbuffer::GapBuffer;
 
 fn main() {
     let mut buffer = GapBuffer::new(30);
     let mut stdout = stdout().into_raw_mode().unwrap();
     let mut index = 0;
-    let mut cx: u16 = 1;
+    let mut cx: u16 = 3;
     let mut cy: u16 = 1;
     write!(stdout,
            "{}{}",
            termion::clear::All,
            termion::cursor::Goto(cx, cy))
-        .unwrap();
+            .unwrap();
     stdout.flush().unwrap();
     'main: loop {
 
-        write!(stdout, "{}", termion::clear::All).unwrap();
-        stdout.flush().unwrap();
+        write!(stdout, "{}", termion::clear::CurrentLine).unwrap();
 
-        draw_lines(&mut stdout);
+        draw_lines(&mut stdout, &buffer.buffer);
         draw_cursor(&mut stdout, cx, cy);
+
+        stdout.flush().unwrap();
 
         for c in std::io::stdin().keys() {
             match c.unwrap() {
                 Key::Char('\n') => {
                     buffer.insert(index, '\n');
-                    cx = 1;
+                    cx = 3;
                     cy += 1;
+                    index += 1;
                 }
                 Key::Char(c) => {
                     buffer.insert(index, c);
@@ -41,12 +44,14 @@ fn main() {
                 }
                 Key::Left => {
                     if cx > 0 {
-                        cx -= 1
+                        cx -= 1;
+                        index -= 1;
                     }
                 }
                 Key::Right => {
                     if cx as usize <= buffer.len() {
-                        cx += 1
+                        cx += 1;
+                        index += 1;
                     }
                 }
                 Key::Esc => break,
@@ -59,20 +64,19 @@ fn main() {
         break;
     }
 }
-fn draw_lines(stdout: &mut termion::raw::RawTerminal<std::io::Stdout>) {
-    // TODO
-    // implement real drawing of buffer lines and line numbers
-    for i in 0..5 {
+fn draw_lines(stdout: &mut termion::raw::RawTerminal<std::io::Stdout>, buffer: &Vec<char>) {
+    let s: String = buffer.iter().collect();
+    for (index, i) in s.split('\n').enumerate() {
         write!(stdout,
-               "{}{}",
-               termion::cursor::Goto(0, i + 1),
-               (i + 1).to_string())
-            .unwrap();
+               "{}{}{}{}",
+               Goto(0, (index + 1) as u16),
+               index,
+               Goto(3, (index + 1) as u16),
+               i)
+                .unwrap();
     }
-    stdout.flush().unwrap();
 }
 
 fn draw_cursor(stdout: &mut termion::raw::RawTerminal<std::io::Stdout>, cx: u16, cy: u16) {
     write!(stdout, "{}", termion::cursor::Goto(cx, cy)).unwrap();
-    stdout.flush().unwrap();
 }
