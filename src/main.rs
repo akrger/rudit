0940f7ebd165be5bfe8188249991f04fa2635309
André Krüger <zeldamer@t-online.de>
2017-09-06 21:02:58 +0200

    update, gapbuffer still not working correctly

--- src/main.rs
+++ src/main.rs
@@ -1,108 +1,61 @@
-extern crate rudit;
+extern crate gapbuffer;
 extern crate termion;
-
 use std::io::{Write, stdout, Stdout};
-
-use termion::raw::IntoRawMode;
+use termion::raw::{IntoRawMode, RawTerminal};
 use termion::event::Key;
 use termion::input::TermRead;
 use termion::cursor::Goto;
-use termion::raw::RawTerminal;
-use rudit::gapbuffer::GapBuffer;
+use gapbuffer::GapBuffer;
 
 fn main() {
-    #![allow(warnings)]
     let mut stdout = stdout().into_raw_mode().unwrap();
-    let mut index = 0;
     let mut cx: u16 = 3;
     let mut cy: u16 = 1;
-    let mut line_num = 1;
-    let mut size = 0;
-
-    let mut file_opened = false;
-    // file_opened = true;
-    if file_opened {
-        size = file_char_count();
-    }
-
-    let mut buffer: GapBuffer = GapBuffer::with_capacity(8);
-
-    if !file_opened {
-        let len = buffer.buf.len() - 1;
-        buffer.buf[len] = '\n';
-        buffer.end -= 1;
-    }
-    if file_opened {
-        line_num = open_file(&mut buffer, line_num);
-        buffer.place_gap(index);
-    }
-    // assert_eq!(buffer.buffer.len(), 3);
-    let (mut line_size, mut start, mut end) = (0, 0, 0);
-    write!(stdout,
-           "{}{}",
-           termion::clear::All,
-           termion::cursor::Goto(cx, cy))
-        .unwrap();
+    let mut buffer = GapBuffer::new(4);
+    write!(stdout, "{}{}", termion::clear::All, termion::cursor::Goto(cx, cy)).unwrap();
     stdout.flush().unwrap();
     'main: loop {
         write!(stdout, "{}", termion::clear::All).unwrap();
-        draw_lines(&mut stdout, &buffer.buf);
-        draw_info(&mut stdout,
-                  index,
-                  line_num,
-                  cx,
-                  cy,
-                  line_size,
-                  start,
-                  end,
-                  buffer.start,
-                  buffer.end);
-        draw_cursor(&mut stdout, cx, cy);
+        let line_len = get_line_len(&buffer);
+        draw_buffer(&mut stdout, &buffer.buffer);
+        draw_info(&mut stdout, line_len);
+        update_cursor(&mut stdout, cx, cy);
         stdout.flush().unwrap();
-
         for c in std::io::stdin().keys() {
             match c.unwrap() {
+
                 Key::Char('\n') => {
-                    buffer.insert(index, '\n');
-                    cx = 3;
+                    buffer.insert('\n');
+                    buffer.point+=1;
                     cy += 1;
-                    index += 1;
-                    line_num += 1;
-                }
+                    cx = 3;
+                },
                 Key::Char(c) => {
-                    buffer.insert(index, c);
-                    index += 1;
+                    buffer.insert(c);
+                    buffer.point+=1;
                     cx += 1;
-                }
-                Key::Backspace => {
-                    // buffer.delete(index);
-                    if index > 0 {
-                        cx -= 1;
-                        index -= 1;
-                    }
-                }
+                },
+                
                 Key::Up => {
-                    if cy > 1 {
-                        cy -= 1;
-                    }
+                    
                 }
                 Key::Down => {
-                    if cy < line_num as u16 - 1 {
-                        cy += 1;
-                        index = 6;
-                    }
+                    
                 }
                 Key::Left => {
-                    if cx > 3 {
+                    if cx >= 4 {                        
                         cx -= 1;
-                        index -= 1;
+                        buffer.point -= 1 
                     }
-                }
+                },
                 Key::Right => {
-                    if cx - 2 < buffer.count() as u16 {
-                        cx += 1;
-                        index += 1;
+                    if cx <= line_len +2    {
+                        cx += 1 ;
+                        buffer.point += 1
                     }
+                },
+                Key::Home => {
+                    buffer.place_gap()
                 }
                 Key::Esc => break,
                 _ => (),
@@ -114,71 +67,41 @@ fn main() {
         break;
     }
 }
-fn draw_info(stdout: &mut RawTerminal<Stdout>,
-             index: usize,
-             line_num: usize,
-             cx: u16,
-             cy: u16,
-             line_count: usize,
-             start: usize,
-             end: usize,
-             gs: usize,
-             ge: usize) {
-    write!(stdout,
-           "{} index {} line_num {} cx {} cy {} line_count {} start   {} end   {}      gs {} ge \
-            {}",
-           termion::cursor::Goto(0, termion::terminal_size().unwrap().1),
-           index,
-           line_num,
-           cx - 2,
-           cy,
-           line_count,
-           start,
-           end,
-           gs,
-           ge)
-        .unwrap();
+fn update_cursor(stdout: &mut RawTerminal<Stdout>, cx: u16, cy: u16) {
+    write!(stdout, "{}", termion::cursor::Goto(cx, cy)).unwrap()
 }
-fn draw_lines(stdout: &mut RawTerminal<Stdout>, buffer: &Vec<char>) {
-    let mut s: String = buffer.iter().collect();
-    // don't draw eof line
-    if s.ends_with('\n') {
-        s.pop();
-    }
-    for (index, i) in s.split('\n').enumerate() {
-        write!(stdout,
-               "{}{}{}{}",
-               Goto(0, (index + 1) as u16),
-               index + 1,
-               Goto(3, (index + 1) as u16),
-               i)
-            .unwrap();
-    }
-}
-
-fn draw_cursor(stdout: &mut RawTerminal<Stdout>, cx: u16, cy: u16) {
-    write!(stdout, "{}", termion::cursor::Goto(cx, cy)).unwrap();
-}
-fn file_char_count() -> usize {
-    use std::io::BufReader;
-    use std::fs::File;
-    use std::io::prelude::*;
-    let mut string = String::from("");
-    BufReader::new(File::open("/home/andre/test2").unwrap()).read_to_string(&mut string).unwrap();
-    string.len()
-}
-fn open_file(buffer: &mut GapBuffer, mut line_num: usize) -> usize {
-    use std::io::BufReader;
-    use std::fs::File;
-    use std::io::prelude::*;
-    let mut string = String::from("");
-    BufReader::new(File::open("/home/andre/test2").unwrap()).read_to_string(&mut string).unwrap();
-    for (index, c) in string.chars().enumerate() {
-        if c == '\n' {
-            line_num += 1;
+fn draw_buffer(stdout: &mut RawTerminal<Stdout>, buffer: &Vec<char>) {
+    let mut lines: u16 = 1;
+    let mut line_start: u16 = 3;
+    for ch in buffer {
+        if *ch == '\n' {
+            lines += 1;
+            line_start = 2
         }
-        buffer.insert(index, c);
+        if *ch != '\0' {
+            write!(stdout, "{}{}", Goto(line_start, lines), ch).unwrap();
+            line_start += 1
+        }
     }
-
-    line_num
+}
+fn get_line_len(buffer: &GapBuffer) -> u16 {
+    let mut line_len = 0;
+    let mut line_start = 0;
+    for i in 0..buffer.point  {
+        if buffer.buffer[i] == '\n' {
+            line_start = i
+        }
+    }
+    for i in line_start..buffer.buffer.len() {
+        if buffer.buffer[i] == '\n' {
+            continue
+        }
+        if buffer.buffer[i] != '\0' {
+            line_len += 1;
+        }
+    }
+    line_len
+}
+fn draw_info(stdout: &mut RawTerminal<Stdout>, line_len: u16) {
+    write!(stdout, "{}{}", Goto(3, 20), line_len).unwrap();
 }
